82d6122cf962314c29109fdabcea9ae86fedf9ca
Nandhini <nandhinigobinath21061996@gmail.com>
2026-09-13 14:54:22 +0530

    Connect frontend to deployed backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Stats from "../components/Stats";
-import DashboardPreview from "../components/DashboardPreview";
+// import Stats from "../components/Stats";
+// import DashboardPreview from "../components/DashboardPreview";
 import Features from "../components/Features";
 import Testimonials from "../components/Testimonials";
 import Footer from "../components/Footer";
